dbb64a5a0bdd228352818b64381e7a981493f294
Nick <ndlyga@syr.edu>
2020-01-20 22:00:19 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# JavaScript Tip Calculator
+
+Let's build a simple JavaScript Tip Calculator! The calculator will need a form that allows the user to input the total bill, the level of service, and the number of people it will then output the total for each person.
+
+## Rubric
+
+- (2pts) Total Bill input is correct with proper label
+- (2pts) Number of People input is correct with proper label
+- (2pts) Service Satisfaction is correct with proper label
+- (4pts) Form is styled correctly
+- (10pts) JavaScript code correctly calculates the total for each person
+
+## Mockup
+
---
+++ calculator.js
@@ -0,0 +1,27 @@
+var total = 0;
+var numberOfPeople = 0;
+var serviceSatisfaction = .20;
+
+// This listens the the form submit event and calls showTip
+document.getElementById('calculatorForm').addEventListener('submit', showTip);
+
+// This listens to the inputs and updates the variables above
+document.querySelector('input[name=total]').addEventListener('change', function(event) {
+    total = parseFloat(event.target.value);
+});
+document.querySelector('input[name=numPeople]').addEventListener('change', function(event) {
+    total = parseFloat(event.target.value);
+});
+document.querySelector('select[name=serviceSatisfaction]').addEventListener('change', function(event) {
+    total = parseFloat(event.target.value);
+});
+
+function showTip(event) {
+    event.preventDefault();
+    tipAmount = 0;
+
+    // You JavaScript Code will go here!
+
+    document.getElementById('tipAmount').innerHTML = 'Each person needs to contribute $' + tipAmount.toString();
+    return false;
+}
